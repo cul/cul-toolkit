ab010892905799bffc3b8c3a9efd363036af8a9e
Eric O <eric@ejdigitalmedia.com>
2024-05-10 14:59:56 -0400

    Deployment config updates

--- config/deploy/stg.rb
+++ config/deploy/stg.rb
@@ -1,4 +1,4 @@
 server "wcm2stgweb01.cc.columbia.edu", user: fetch(:remote_user), roles: %w{web app}
 server "wcm2stgweb02.cc.columbia.edu", user: fetch(:remote_user), roles: %w{web app}
-ask :branch, proc { `git tag --sort=version:refname`.split("\n").last }
+ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 set :tmp_dir, "/u/7/c/culwcm/capistrano_tmp"
