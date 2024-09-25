1c440bd52d3551a2e46c6d66346ec6b944fb47e0
erik r <er2576@columbia.edu>
2024-09-24 21:39:30 -0400

    WIP build culmenu from cul-main-menu.json file!

--- src/js/main.js
+++ src/js/main.js
@@ -6,3 +6,7 @@ import '../scss/styles.scss'
 // Import all of Bootstrap's JS
 import * as bootstrap from 'bootstrap'
 
+// import our CUL menu from JSON builder
+import { makeCULmenu } from './culmenu.js'
+makeCULmenu('main-menu');
+
