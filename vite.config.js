035b01ae8a395cc49c6ec9b8493086d1a68ae085
erik r <er2576@columbia.edu>
2024-09-26 00:00:36 -0400

    WIP add and imprement handlebars for index and examples

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,34 @@
 import { resolve } from 'path'
 import { defineConfig } from 'vite'
+import handlebars from 'vite-plugin-handlebars';
+
+const pageData = {
+  '/index.html': {
+    title: 'cul-toolkit',
+  },
+  '/examples/index.html': {
+    title: 'cul-toolkit &raquo; examples',
+  },
+  '/examples/blogs-index.html': {
+    title: 'cul-toolkit &raquo; examples &raquo; CUL Blogs Example Landing Page',
+  },
+};
 
 export default defineConfig({
   root: resolve(__dirname, 'src'),
   base: '',
+  plugins: [
+	handlebars({
+      //partialDirectory: resolve(__dirname, './partials'),
+      partialDirectory: [ 
+        resolve(__dirname, 'src','partials'),
+        resolve(__dirname, 'src','examples'),
+      ],
+	  context(pagePath) {
+		return pageData[pagePath];
+	  },
+    }),
+  ],
   build: {
     outDir: '../dist',
     emptyOutDir: true,
