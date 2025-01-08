05838b51316d4f36663aea5adb7e57bd8e577ca3
erik r <er2576@columbia.edu>
2025-01-08 13:35:25 -0500

    WIP staffweb examp

--- vite.config.js
+++ vite.config.js
@@ -28,10 +28,10 @@ const pageData = {
   '/examples/staffweb-home.html': {
     title: 'cul-toolkit &raquo; examples &raquo; CUL StaffWeb Example Landing Page',
 	searchHero: true,
-	searchHeroPlaceholder: 'Search StaffWeb',
+	searchHeroPlaceholder: '&#x1F50D; Search StaffWeb&hellip;',
 	stylesheet: ['_example-styles.scss', '_staffweb-styles.scss'],
-	whichHero: '_slimhero',
-	heroTitle: 'StaffWeb',
+	//whichHero: '_slimhero',
+	//heroTitle: 'StaffWeb',
   },
 };
 
