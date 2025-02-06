cd5cb964fe65503e9d4bbaac7d39b0eb05fa58b1
erik r <er2576@columbia.edu>
2025-02-06 18:03:43 -0500

    WIP new staffweb examples. cleanups/refactorings.

--- vite.config.js
+++ vite.config.js
@@ -9,31 +9,37 @@ const pageData = {
   },
   '/examples/index.html': {
     title: 'cul-toolkit &raquo; examples',
-	whichHero: '_slimhero',
-	stylesheet: ['_example-styles.scss'],
-	heroTitle: 'Columbia University Libraries',
+    whichHero: '_slimhero',
+    stylesheet: ['_example-styles.scss'],
+    heroTitle: 'Columbia University Libraries',
   },
   '/examples/blogs-index.html': {
     title: 'cul-toolkit &raquo; examples &raquo; CUL Blogs Example Landing Page',
-	whichHero: '_slimhero',
-	stylesheet: ['_example-styles.scss', '_blogs-styles.scss'],
-	heroTitle: 'Columbia University Libraries Blogs',
+    whichHero: '_slimhero',
+    stylesheet: ['_example-styles.scss', '_blogs-styles.scss'],
+    heroTitle: 'Columbia University Libraries Blogs',
   },
   '/examples/lweb-home.html': {
     title: 'cul-toolkit &raquo; examples &raquo; CUL LWeb Example Landing Page',
-	searchHero: true,
-	searchHeroPlaceholder: '&#x1F50D; Search CLIO & the Libraries&hellip;',
-	stylesheet: ['_example-styles.scss', '_lweb-styles.scss'],
-	heroTitle: 'Columbia University Libraries',
+    searchHero: true,
+    searchHeroPlaceholder: '&#x1F50D; Search CLIO & the Libraries&hellip;',
+    stylesheet: ['_example-styles.scss', '_lweb-styles.scss'],
+    heroTitle: 'Columbia University Libraries',
   },
   '/examples/staffweb-home.html': {
     title: 'cul-toolkit &raquo; examples &raquo; CUL StaffWeb Example Landing Page',
-	searchHero: true,
-	searchHeroPlaceholder: '&#x1F50D; Search StaffWeb pages&hellip;',
-	searchInputAddClasses: 'rounded-pill',
-	stylesheet: ['_example-styles.scss', '_staffweb-styles.scss'],
-	//whichHero: '_slimhero',
-	//heroTitle: 'StaffWeb',
+    searchHero: true,
+    searchHeroPlaceholder: '&#x1F50D; Search StaffWeb pages&hellip;',
+    searchInputAddClasses: 'fs-5',
+    stylesheet: ['_example-styles.scss', '_staffweb-styles.scss'],
+  },
+  '/examples/staffweb-inner-hr.html': {
+    title: 'cul-toolkit &raquo; examples &raquo; CUL StaffWeb Example Inner HR Home',
+    stylesheet: ['_example-styles.scss', '_staffweb-styles.scss'],
+  },
+  '/examples/staffweb-inner-hr-jobs.html': {
+    title: 'cul-toolkit &raquo; examples &raquo; CUL StaffWeb Example Inner HR Jobs Page',
+    stylesheet: ['_example-styles.scss', '_staffweb-styles.scss'],
   },
 };
 
@@ -41,16 +47,16 @@ export default defineConfig({
   root: resolve(__dirname, 'src'),
   base: '',
   plugins: [
-	handlebars({
-      partialDirectory: [ 
-        resolve(__dirname, 'src','partials'),
-        resolve(__dirname, 'src','examples'),
+    handlebars({
+      partialDirectory: [
+        resolve(__dirname, 'src', 'partials'),
+        resolve(__dirname, 'src', 'examples'),
       ],
-	  context(pagePath) {
-		let pData = pageData[pagePath];
-		pData.culmenu = culmenu;
-		return pData;
-	  },
+      context(pagePath) {
+        let pData = pageData[pagePath];
+        pData.culmenu = culmenu;
+        return pData;
+      },
       helpers: {
         lowerdash: (str) => {
           return str.fn(this).replace(/\s+/g, '-').toLowerCase()
@@ -59,7 +65,7 @@ export default defineConfig({
       runtimeOptions: {
         // config option: define custom private @variables
         data: {
-			apptitle: 'cul-toolkit',
+          apptitle: 'cul-toolkit',
         },
       },
     }),
@@ -74,6 +80,7 @@ export default defineConfig({
         blogIndex: resolve(__dirname, 'src/examples/blogs-index.html'),
         lwebIndex: resolve(__dirname, 'src/examples/lweb-home.html'),
         staffIndex: resolve(__dirname, 'src/examples/staffweb-home.html'),
+        staffInnner: resolve(__dirname, 'src/examples/staffweb-inner-hr.html'),
       },
       output: {
         //entryFileNames: `assets/[name].js`,
