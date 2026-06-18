4c793f58eb5d6ec2956c1ac55c44d72b84f0850b
Eric O <eric@ejdigitalmedia.com>
2026-06-18 12:31:10 -0400

    Make bootstrap variable globally available so that we can control Boostrap JS-backed components (like the Carousel)

--- src/js/main.js
+++ src/js/main.js
@@ -9,6 +9,9 @@ import * as bootstrap from 'bootstrap';
 import { makeCULmenu } from './culmenu.js';
 import { makeCULNavbarMenu } from './culmenu-navbar.js';
 
+// Make bootstrap variable globally available so that we can control Boostrap JS-backed components (like the Carousel)
+window.bootstrap = bootstrap;
+
 const MENU_URL =
   import.meta.env.VITE_CUL_MENU_URL || undefined;
 
